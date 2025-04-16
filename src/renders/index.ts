d54c22d5a7bf880ed87e1dc7a04738c9eaced400
vace <vace@users.noreply.github.com>
2025-04-16 14:45:34 +0000

    deploy: 54a59f3ad83552e9ae577fd4e9f3d8553d962a8d

--- src/renders/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './render-blocks'
-export * from './render-text'
-export * from './render-tokens'
-export * from './render-paragraph'
-export * from './render-list'
-export * from './render-checkbox'
-export * from './render-table'
-
--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
-    globals: true,
-    environment: 'node',
-  },
-}) 
